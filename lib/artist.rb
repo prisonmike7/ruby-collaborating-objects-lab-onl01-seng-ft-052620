08381bad8ee57973fed3f537e7ccc796e9296fdc
Michael Harris <mwharris7@gmail.com>
2020-06-10 08:05:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,14 +26,13 @@ class Artist
     end
   end
 
-  # def self.find_or_create_by_name(name)
-  #   @@all.each do |artist|
-  #     if artist.name == name
-  #       artist
-  #       return artist
-  #     else
-  #       self.new(name)
-  #     end
-  #   end
-  # end
+  def self.find_or_create_by_name(name)
+    @@all.each do |artist|
+      if artist.name == name
+        return artist
+      else
+        self.new(name)
+      end
+    end
+  end
 end
