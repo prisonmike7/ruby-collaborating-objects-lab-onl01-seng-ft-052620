123f581a5c0a9a23ef454a3c96fe2fe8ebf746bc
Michael Harris <mwharris7@gmail.com>
2020-06-10 04:18:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,13 @@ class Artist
 
   def add_song(song)
     song.artist = self
+  end
 
+  def songs
+    new_array = []
+    Song.all.each do |song|
+      new_array << song if song.artist.name == self.name
+    end
   end
 
 end
